7cf7e6ccf9ff4c429a91b4aadb8c55938eb61922
Arlyn Viray <viray.arlyn@gmail.com>
2018-07-17 17:58:14 -0700

    added to client and server files

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem 
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggleNavbar = this.toggleNavbar.bind(this);
+        this.state = {
+          collapsed: true
+        };
+      }
+    
+      toggleNavbar() {
+        this.setState({
+          collapsed: !this.state.collapsed
+        });
+      }
+      render() {
+        return (
+          <div>
+            <Navbar color="faded" light>
+              <NavbarBrand href="/" className="mr-auto">Blog App</NavbarBrand>
+              <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+              <Collapse isOpen={!this.state.collapsed} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink href="/">Login</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">registier</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="/">view posts</NavLink>
+                  </NavItem>
+                </Nav>
+              </Collapse>
+            </Navbar>
+          </div>
+        );
+      }
+    }
+
+
+
+export default AppNavbar;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://admin:admin123@ds239681.mlab.com:39681/merngroupproject'
+}
