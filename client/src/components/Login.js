5eeabf31e4296dacabbbc6b5d94e4242c91b4929
Robert Bender <robertbender@Roberts-MacBook-Pro.local>
2018-07-21 12:03:27 -0700

    added new dependencies, updated server.js for mongooseDB, URI, heroku; updated client login, footer, routes  components.

---
+++ client/src/components/Login.js
@@ -0,0 +1,41 @@
+import React from "react";
+import './index.css';
+// import './Login.css';
+import { Form, FormControl, FormGroup, Col, Button } from 'react-bootstrap';
+
+class Login extends React.Component {
+
+  render() {
+    return(
+      <div className="Login">
+        <h1>Login</h1>
+        <Form horizontal action="/login" method="post">
+          <FormGroup controlId="formHorizontalUser">
+          <Col sm={2}>
+          Username:
+          </Col>
+          <Col sm={10}>
+          <FormControl type="text" name="username" />
+          </Col>
+          </FormGroup>
+          <FormGroup controlId="formHorizontalPassword">
+          <Col sm={2}>
+          Password:
+          </Col>
+          <Col sm={10}>
+          <FormControl type="password" name="password" />
+          </Col>
+          </FormGroup>
+          <FormGroup>
+            <Col sm={12}>
+          <Button bsStyle="info" type="submit">Submit</Button>
+          </Col>
+          </FormGroup>
+        </Form>
+        <Col sm={12}>
+        <p>Don`'`t have an account yet? <a className="loginLinkStyle" href="#signup">Sign up here</a></p>
+        </Col>
+      </div>
+    );
+  }
+}
--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const morgan = require('morgan');
 const app = express();
+const db = mongoose.connection;
+
+const posts = require('./routes/api/posts');
 
 // PRODUCTION ONLY
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // app middleware
 app.use(bodyParser.json())
@@ -14,34 +21,29 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 // PRODUCTION ONLY
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // Development mode port
-const port = process.env.PORT || 5000;
-app.listen(port)
+// const port = process.env.PORT || 5000;
+// app.listen(port)
 
-module.exports = app;
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+//Mongoose connect to db
+mongoose.connect('mongodb://admin:adminADMIN1234@ds141671.mlab.com:41671/blog_7-16-a', function(err) {
+  if (err) {
+    console.err(err);
+  } else {
+    console.log('Connected');
+  }
+});
 
-const posts = require('./routes/api/posts');
-
-const app = express();
-
-//Bodyparser Middleware
-app.use(bodyParser.json());
-
-//DB Config
-const db = require('./config/keys').mongoURI;
-
-//Connect to Mongo
-mongoose
-    .connect(db) 
-    .then(() => console.log('Sweet! DB Connected!'))
-    .catch(err => console.log(err));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('DB connected!');
+});
+mongoose.set('bufferCommands', false);
+mongoose.set('debug', true);
 
 //Use Routes
 app.use('/api/posts', posts);
@@ -49,3 +51,5 @@ app.use('/api/posts', posts);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+module.exports = app;
